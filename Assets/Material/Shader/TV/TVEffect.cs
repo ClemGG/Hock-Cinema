be989d798207cca1036a135fef8ecc02d2b65268
Clement Guichard <c.guichard508@laposte.net>
2020-06-23 18:03:29 +0200

    push assets no 3 (meshes)

---
+++ Assets/Material/Shader/TV/TVEffect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TVEffect : MonoBehaviour
+{
+    [SerializeField] private Material Mat;
+    Material _mat;
+    private void Start()
+    {
+        _mat = new Material(Mat);
+    }
+
+
+    public void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, _mat);
+    }
+
+
+    public Material _Mat
+    {
+        get { return Mat; }
+        set { Mat = value; }
+    }
+}

62b1f49385f2fd0e18abcae7953188aaf578429a
Clement Guichard <c.guichard508@laposte.net>
2020-06-24 16:02:35 +0200

    push asset no 4

---
+++ Assets/Prefab/Clement/Persos/testAnimPerso.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testAnimPerso : MonoBehaviour
+{
+    public Animator a;
+    public string[] animNames;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < animNames.Length; i++)
+        {
+            if (Input.GetKeyDown(i.ToString()))
+            {
+                a.Play(animNames[i]);
+            }
+        }
+    }
+}
